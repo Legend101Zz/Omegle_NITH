ef23e1d28f82ab4bd25897a5b6f601343c282c7e
ice-009 <21bma009@nith.ac.in>
2022-11-13 16:03:27 +0530

    email added

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,6 +1,30 @@
 const User = require("../../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+// const transporter= require("../../routes/nodemailer")
+// const sgmail= require('@sendgrid/mail')
+// const api_key= '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'
+
+// sgmail.setApiKey(api_key)
+//     const message = {
+//       to: 'armaanshukla06@gmail.com',
+//       from: '21bma009@nith.ac.in',
+//       subject: "hello",
+//       text: "hello",
+//       html : '<h1>Hello</h1>'
+//     }
+//     sgmail.send(message)
+//     .then(response=>{
+//       console.log('Email sent')
+//     }).catch(err=>{
+//       console.log(err)
+//     })
+const Sib= require('sib-api-v3-sdk')
+require('dotenv').config()
+const client= Sib.ApiClient.instance
+const apiKey = client.authentications['api-key']
+apiKey.apiKey = process.env.API_KEY
+
 
 const postRegister = async (req, res) => {
   try {
@@ -18,13 +42,40 @@ const postRegister = async (req, res) => {
 
     // encrypt password
     const encryptedPassword = await bcrypt.hash(password, 10);
+    const tranEmailApi = new Sib.TransactionalEmailsApi()
+    const sender = {
+      email: '21bma009@nith.ac.in',
+      name: 'Armaan',
+  }
+  const receivers = [
+      {
+          email: 'thearmaanshukla@gmail.com'
+      },
+  ]
+  
 
     // create user document and save in database
     const user = await User.create({
       username,
       mail: mail.toLowerCase(),
       password: encryptedPassword,
-    });
+    }).then(tranEmailApi
+    .sendTransacEmail({
+        sender,
+        to: receivers,
+        subject: 'Hello',
+        textContent: `
+        developer.
+        `,
+        htmlContent: `
+        <h1>hey</h1>
+        
+                `,
+    })
+    .then(console.log)
+    .catch(console.log))
+    
+   
 
     // create JWT token
     const token = jwt.sign(
@@ -36,7 +87,20 @@ const postRegister = async (req, res) => {
       {
         expiresIn: "24h",
       }
-    );
+    )
+    // .then(result=>{
+    //   transporter.sendMail({
+    //   to: 'armaanshukla06@gmail.com' ,
+    //   from: '21bma009@nith.ac.in',
+    //   subject: 'Fuck you',
+    //   html :' <h1>HEllo murgi</h1>'
+    // });
+
+    // }).catch(err=>{
+    //   console.log(err)
+    // })
+    
+    // sgmail.send(message)
 
     res.status(201).json({
       userDetails: {
@@ -44,7 +108,8 @@ const postRegister = async (req, res) => {
         token: token,
         username: user.username,
       },
-    });
+    })
+    
   } catch (err) {
     return res.status(500).send("Error occured. Please try again");
   }
